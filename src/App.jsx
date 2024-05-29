fd3d3533457e6f8c2e76379a133912d8c3e3028d
R. Abdurrohman <rifaiabdurrohman8@gmail.com>
2024-05-29 10:41:41 +0700

    save data

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,27 @@
-import { useRef } from "react";
+import { useRef, useState } from "react";
 import "./App.css";
 import Form from "./components/Form";
 import Todolist from "./components/Todolist";
 
 function App() {
   const newTask = useRef("");
+  const [tasks, setTasks] = useState([]);
+
   function addTask(parameter) {
     parameter.preventDefault();
-    console.info(`I'm Clicked value ${newTask.current.value} `);
+    // code mewajibkan isi value
+    if (newTask.current.value === "") {
+      alert("Silahkan di isi!");
+      return false;
+    }
+    const data = {
+      id: 1,
+      task: newTask.current.value,
+      completed: false,
+    };
+    setTasks(data);
+    console.info(tasks);
+    // console.info(`I'm Clicked, value = ${newTask.current.value} `);
   }
   return (
     <>
